11423de08ba79e71bfa832f188a6d6a719d7eeee
elinepeeters1 <e.w.h.peeters@student.utwente.nl>
2025-01-09 10:57:31 +0100

    Toevoeging game id (basis), score per speler, en bom

--- __main__.py
+++ __main__.py
@@ -6,10 +6,20 @@ def split_dealing_turn(head: str):
     gr_cards_lines_split = gr_cards_lines_other_split[0].split("\n")
     start_cards_lines_other_split = gr_cards_lines_other_split[1].split("Schupfen:\n", 1)
     start_cards_lines_split = start_cards_lines_other_split[0].split("\n")[:-1]  # [:-1] to get rid of the empty string
-    deal_lines_split = start_cards_lines_other_split[1].split("\n", 4)[:4]
-    return gr_cards_lines_split, start_cards_lines_split, deal_lines_split
+    deal_lines_split = start_cards_lines_other_split[1].split("\n")[:-1]  # [:-1] to get rid of the empty string
 
-def starting_turn_information(gr_cards_line: str, start_cards_line: str, deal_line: str, hands: list[set[str]], player_id: int) -> list[str]:
+    bombs_player_id = []
+    if "BOMBE" in deal_lines_split[-1]:
+        bombs_players = deal_lines_split[-1].split(":")[1].strip().split(" ")
+        bombs_player_id = [int(player[1]) for player in bombs_players]
+        print(bombs_player_id)
+        deal_lines_split = deal_lines_split[:4]
+
+    return gr_cards_lines_split, start_cards_lines_split, deal_lines_split, bombs_player_id
+
+
+def starting_turn_information(gr_cards_line: str, start_cards_line: str, deal_line: str, hands: list[set[str]],
+                              player_id: int) -> list[str]:
     row = []
 
     player_gr_cards_split = gr_cards_line.split(" ", 1)
@@ -37,16 +47,18 @@ def starting_turn_information(gr_cards_line: str, start_cards_line: str, deal_li
 
     return row
 
+
 def get_gr_tichu_callers(start_cards_lines_split: list[str]) -> set[int]:
     # from the example of the head you can see the grand tichu calls are after the 4 'startkarten' rows
     gr_tichu_called = len(start_cards_lines_split) > 4  # because there are 4 lines + possible Grand Tichu callers
     gr_tichu_callers: set[int] = set()
     return set(gr_tichu_callers.add(int(line[16])) for line in start_cards_lines_split[4:] if gr_tichu_called)
 
+
 def other_turns_information(turns: list[str], hands: list[set[str]]):
     rows: list[list[any]] = [[], [], [], []]
     tichu_callers: set[int] = set()
-    finished_position = 1.0 
+    finished_position = 1.0
     finished_players: set[int] = set()
     game_ended = False
     for i, line in enumerate(turns):
@@ -59,9 +71,9 @@ def other_turns_information(turns: list[str], hands: list[set[str]]):
 
         if line[:7] == "Wunsch:":
             wish_called = line.split(":", 1)[1]
-            wisher_id = int(turns[i-1][1])  # the previous line holds the id of the person who wishes a card
+            wisher_id = int(turns[i - 1][1])  # the previous line holds the id of the person who wishes a card
             rows[wisher_id].append(wish_called)
-            for id_ in set({0,1,2,3}) - set({wisher_id}):
+            for id_ in set({0, 1, 2, 3}) - set({wisher_id}):
                 rows[id_].append(None)
             continue
 
@@ -78,44 +90,57 @@ def other_turns_information(turns: list[str], hands: list[set[str]]):
                 rows[player_id].append(finished_position)
                 finished_position += 1
                 finished_players.add(player_id)
-            
-            if len(finished_players) == 2 and (finished_players == set({0,2}) or finished_players == set({1,3})):
-                for id in set({0,1,2,3}) - finished_players:
+
+            if len(finished_players) == 2 and (finished_players == set({0, 2}) or finished_players == set({1, 3})):
+                for id in set({0, 1, 2, 3}) - finished_players:
                     rows[id].append(3.5)
                 game_ended = True
                 break
             elif len(finished_players) == 3:
-                missing_id = (set({0,1,2,3}) - finished_players).pop()
+                missing_id = (set({0, 1, 2, 3}) - finished_players).pop()
                 rows[missing_id].append(4.0)
                 game_ended = True
                 break
-    
+
     if all(len(row) == 1 for row in rows):
         for row in rows:
             row.append(None)
 
     return rows, tichu_callers
 
+
+def score(score_line: str):
+    if "Ergebnis" not in score_line:
+        print('Ergebnis niet gevonden in score_line')
+    scores = score_line.split(":", 1)[1].strip()
+    [scores_even, scores_uneven] = list(map(lambda x: int(x.strip()), scores.split(" - ")))
+    return scores_even, scores_uneven
+
+
 def csv_rows(game: str) -> list[str]:
     head_other_split = game.split("---------------Rundenverlauf------------------\n", 1)
     head = head_other_split[0]
     body = head_other_split[1]
-    gr_cards_lines_split, start_cards_lines_split, deal_lines_split = split_dealing_turn(head)
+
+    gr_cards_lines_split, start_cards_lines_split, deal_lines_split, bombs_player_id = split_dealing_turn(head)
 
     # loop over the 4 players (with ids 0,1,2,3) and retrieve their starting turn information
     hands: list[set[str]] = [set(), set(), set(), set()]
     rows: list[list[any]] = []
     for id_ in range(4):
-        rows.append(starting_turn_information(gr_cards_lines_split[id_], start_cards_lines_split[id_], deal_lines_split[id_], hands, id_))
-    
+        rows.append(
+            starting_turn_information(gr_cards_lines_split[id_], start_cards_lines_split[id_], deal_lines_split[id_],
+                                      hands, id_))
+
     gr_tichu_callers = get_gr_tichu_callers(start_cards_lines_split)
-    for id_ in set({0,1,2,3}):
+    for id_ in set({0, 1, 2, 3}):
         if id_ in gr_tichu_callers:
             rows[id_].append(1)
         else:
-            rows[id_].append(0)        
+            rows[id_].append(0)
 
-    new_rows, tichu_callers = other_turns_information(body.strip().split("\n")[:-1], hands)  # leave out final line including scores
+    new_rows, tichu_callers = other_turns_information(body.strip().split("\n")[:-1],
+                                                      hands)  # leave out final line including scores
     for id_, row in enumerate(rows):
         rows[id_] += new_rows[id_]
 
@@ -124,25 +149,39 @@ def csv_rows(game: str) -> list[str]:
         else:
             row.append(0)
 
-    return rows    
+    for row in rows:  # game id
+        row.append(1)
+
+    scores_even, scores_uneven = score(body.strip().split("\n")[-1])
+    for id, row in enumerate(rows):  # score per player
+        if id % 2 == 0:
+            row.append(scores_even)
+        else:
+            row.append(scores_uneven)
+
+    for id, row in enumerate(rows):
+        if id in bombs_player_id:
+            row.append(1)
+        else:
+            row.append(0)
+
+    return rows
 
 
 if __name__ == "__main__":
     with open("./columns.txt") as f:
         columns = [col.replace("\n", "") for col in f.readlines()]
-    
+
     rows = []
     with open("./tichulog.txt") as f:
         games = f.read().split("---------------Gr.Tichukarten------------------\n")[1:]  # the first is an empty string
-        
+
         for game in games:
             game_rows = csv_rows(game)
             for row in game_rows:
                 rows.append(row)
 
-        
-
-    with open('rows.csv', 'w', newline='') as f:
+    with open('rows_eline_haroun.csv', 'w', newline='') as f:
         write = csv.writer(f)
         write.writerow(columns)
         write.writerows(rows)
